a3f18f817bcf437f359a95994c684639fa51448b
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-06-23 15:25:05 +0200

    add: insert method for the hashtable + docs

--- src/hash_table.rs
+++ src/hash_table.rs
@@ -34,12 +34,25 @@ where
         }
     }
 
-    pub fn get(&self, key: Key) -> Option<&Value> {
+    /// Private function that finds a slot for the given key
+    /// 
+    /// Uses the hashed value of the key.
+    /// Uses open addressing to find the key slot that is not taken. 
+    fn find_slot(&self, key: &Key) -> usize{
         let mut index = key.hash() % &self.table_length;
 
-        while !self.buckets[index].taken && self.buckets[index].key != key {
+        while self.buckets[index].taken && self.buckets[index].key != *key {
             index = (index + 1) % self.table_length;
         }
+
+        index
+    }
+
+    /// Get value (as reference) from the key
+    /// 
+    /// Returns an option based on if the key is present in the `HashTable`
+    pub fn get(&self, key: &Key) -> Option<&Value> {
+        let index = self.find_slot(key);
 
         if self.buckets[index].taken {
             return Some(&self.buckets[index].value);
@@ -48,12 +61,11 @@ where
         return None;
     }
 
-    pub fn get_mut(&mut self, key: Key) -> Option<&mut Value> {
-        let mut index = key.hash() % &self.table_length;
-
-        while !self.buckets[index].taken && self.buckets[index].key != key {
-            index = (index + 1) % self.table_length;
-        }
+    /// Get a mutable reference to the value, given the key.
+    /// 
+    /// Returns an option based on if the key is present in the `HashTable`
+    pub fn get_mut(&mut self, key: &Key) -> Option<&mut Value> {
+        let index = self.find_slot(key);
 
         if self.buckets[index].taken {
             return Some(&mut self.buckets[index].value);
@@ -62,7 +74,27 @@ where
         return None;
     }
 
+    /// Insert a new key value pair into the `HashTable`
+    /// 
+    /// Will extend the `HashTable` if there is not enough room for new elements.
+    /// We extend the if there is the `HashTable` is filled more than 75% of its capacity
     pub fn insert(&mut self, key: Key, value: Value) {
+        let index = self.find_slot(&key);
+
+        // Check if we need to extend the hash table 
+        if self.taken_count + 1 > self.table_length * 3/4{
+            self.extend()
+        }
+
+        // Set value 
+        self.buckets[index].taken = true;
+        self.buckets[index].key = key;
+        self.buckets[index].value = value;
+
+        self.taken_count += 1
+    }
+
+    fn extend(&mut self){
         todo!()
     }
 
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,52 @@ pub mod hash_table;
 pub mod hash_table_entry;
 pub mod hash_trait;
 
+fn print_divider(){
+    println!("############################ \n"); 
+}
+
+fn print_get_result(key: String, op: Option<&String>){
+    if let Some(item) = op{
+        println!("GET:{:?} => {:?}", key, item);
+    }else{
+        println!("GET:{:?} => NONE", key);
+    }
+}
+
 fn main() {
     // Demo of usage
-    let table: HashTable<String, String> = HashTable::with_capacity(20);
+    let mut table: HashTable<String, String> = HashTable::with_capacity(10);
 
     // Print the content of the hash table
     table.print();
+
+    // Print divider
+    print_divider();
+
+    // Inserting a value
+    table.insert("12".to_string(), "AWS".to_string());
+    table.insert("11".to_string(), "Microsoft".to_string());
+    // Printing the new table content
+    table.print();
+
+    // Print divider
+    print_divider();
+
+    // Retrieve an element that does not exist 
+    let key_google = "10".to_string();
+    let google_item: Option<&String> = table.get(&key_google);
+    print_get_result(key_google, google_item);
+
+    // Retrieving an item that does exist 
+    let key_aws = "12".to_string();
+    let aws_item: Option<&String> = table.get(&key_aws);
+    print_get_result(key_aws, aws_item);
+
+    // Print divider
+    print_divider();
+
+    // Changing a value 
+
+
+
 }
