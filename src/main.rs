39dff6d27c5b687b171a0a2815320a7941828c9d
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-06-23 14:09:00 +0200

    add: main with capacity

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-struct HashTable<T>{
-    buckets: Vec<T>,   
-}
+use hash_table::HashTable;
 
+pub mod hash_trait;
+pub mod hash_table;
+pub mod hash_table_entry;
 
 fn main() {
     println!("Hello, world!");
+
+    // Demo of usage 
+    let _table:HashTable<String, String> = HashTable::with_capacity(20);
 }
