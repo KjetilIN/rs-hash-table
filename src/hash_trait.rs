820cbc12482fe8121af9b1f78770fd89572ced13
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-06-23 14:08:08 +0200

    add: hash trait for djb2 hashing

---
+++ src/hash_trait.rs
@@ -0,0 +1,15 @@
+pub trait Hash {
+    fn hash(&self) -> usize;
+}
+
+impl Hash for String {
+    fn hash(&self) -> usize {
+        let mut hash = 5381;
+
+        for byte in self.as_bytes(){
+            hash = ((hash << 5) + hash) + (*byte as usize);
+        }
+
+        hash
+    }
+}
